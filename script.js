ce89995e58ad420a793139355a5734c77abd968e
Luis Cardona <cardona-luis@outlook.com>
2024-08-08 19:43:53 -0500

    LoadFaster

--- script.js
+++ script.js
@@ -1,33 +1,20 @@
 // alert("hello js file")
 
-const FullReviewHandler = (e)=>{
-    console.log(e.target.id)
-
-    if(e.target.innerHTML === "Close"){
-        document.getElementById(e.target.id).innerHTML = "Read Full Review";
-    document.getElementById("Review" +e.target.id).style.height = "200px";
-
-    }else{
-
-        document.getElementById(e.target.id).innerHTML = "Close";
-    document.getElementById("Review" +e.target.id).style.height = "700px";
-
-    }
-    
-
-}
-
-
 document.addEventListener('DOMContentLoaded', async () => {
     try {
-        const response1 = await fetch('videoContact.html');
-        const html1 = await response1.text();
-        document.getElementById('videoContact').innerHTML = html1;
 
         const response2 = await fetch('reviews.html');
         const html2 = await response2.text();
         document.getElementById('reviewsContent').innerHTML = html2;
+        
+        const response1 = await fetch('videoContact.html');
+        const html1 = await response1.text();
+        document.getElementById('videoContact').innerHTML = html1;
+
+       
     } catch (error) {
         console.error('Error loading content:', error);
     }
 });
+
+
